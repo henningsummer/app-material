e646fe04e775408c66654b2191935436c72c0ac4
Henning Summer <henningsummer3@gmail.com>
2021-04-26 00:48:30 -0300

    ajuste das rotas

--- js/routes.js
+++ js/routes.js
@@ -6,28 +6,44 @@ $(document).ready(function () {
     }, 800);
 });
 
+const rotas = ['pages/info.html', 'pages/listagem.html']
+
+function muda_rota(rota) {
+    console.log(`info ${rota}`);
+    $("#content").html('')
+    $(".progress").css('display', 'block')
+    $(".sidenav-overlay").click()
+    setTimeout(() => {
+        $("#content").load(rota);
+        $(".progress").css('display', 'none')
+    }, 2000);
+}
+
 
 
 
 function infoPage() {
-    console.log('info page');
-    $("#content").html('')
-    $(".progress").css('display', 'block')
-    $(".sidenav-overlay").click()
-    setTimeout(() => {
-        $("#content").load('pages/info.html');
-        $(".progress").css('display', 'none')
-    }, 2000);
+    // console.log('info page');
+    // $("#content").html('')
+    // $(".progress").css('display', 'block')
+    // $(".sidenav-overlay").click()
+    // setTimeout(() => {
+    //     $("#content").load('pages/info.html');
+    //     $(".progress").css('display', 'none')
+    // }, 2000);
+    muda_rota(rotas[0])
 
 };
 function homePage() {
-    console.log('info page');
-    $("#content").html('')
-    $(".progress").css('display', 'block')
-    $(".sidenav-overlay").click()
-    setTimeout(() => {
-        $("#content").load('pages/listagem.html');
-        $(".progress").css('display', 'none')
-    }, 2000);
+    // console.log('info page');
+    // $("#content").html('')
+    // $(".progress").css('display', 'block')
+    // $(".sidenav-overlay").click()
+    // setTimeout(() => {
+    //     $("#content").load('pages/listagem.html');
+    //     $(".progress").css('display', 'none')
+    // }, 2000);
+
+    muda_rota(rotas[1])
 
 };
